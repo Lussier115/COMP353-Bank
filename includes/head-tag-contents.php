88c3ed2cbb504e9f03a03e7eff6c1eb34b32ec11
William-Andrew Lussier <william.lussier115@hotmail.com>
2018-11-02 12:26:27 -0400

    Revert "Merge branch 'master' into dev"
    This reverts commit 464c1d80ea823c14ea90cc0d40b0af9ecc8721e3.

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -5,9 +5,6 @@
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 <style>
-	body {
-		font-family: monospace;
-	}
 	#main-content {
 		margin-top:20px;
 	}
@@ -21,7 +18,8 @@
 
 	.header {
 		margin-bottom: 2rem;
-    	background-color: rgba(55,55,55,.15);
-    	padding: 3rem 2rem;
+    	background-color: #e9ecef;
+    	border-radius: .3rem;
+		padding: 4rem 2rem;
 	}
 </style>
