8cbaa086ceef877fd1e5e5ce208f7940c3d46005
William-Andrew Lussier <william.lussier115@hotmail.com>
2018-11-14 10:19:03 -0500

    Only admins can create employee

--- authentication/create-account.php
+++ authentication/create-account.php
@@ -55,25 +55,9 @@
     <?php include("../includes/header.php"); ?>
         <div class="container" id="main-content">
             <h2>Create Account</h2>
-            
-            
-            <form action = "" method = "post" class = "form-box">
-                <h3>For Employees</h3>
-                <label>Email  :</label><input type = "email_address" name = "email_address" class = "box"/><br /><br />
-                <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br/>
-                <label>First Name  :</label><input type = "text" name = "first_name" class = "box"/><br /><br />
-                <label>Last Name  :</label><input type = "text" name = "last_name" class = "box"/><br /><br />
-                <label>Title  :</label><input type = "text" name = "title" class = "box"/><br /><br />
-                <label>Phone Number :</label><input type = "tel" name = "phone" class = "box"/><br /><br />
-                <label>Start Date  :</label><input type = "date" name = "start_date" class = "box"/><br /><br />
-                <label>Address :</label><input type = "text" name = "address" class = "box"/><br /><br />
-                <label>Salary  :</label><input type = "number" name = "salary" class = "box"/><br /><br />
-                <label>isAdmin  :</label><input type = "checkbox" name = "is_admin" class = "box" /><br/><br/>
-                <input type="hidden" name="action" value="employee">
-                <input type = "submit" value = " Submit "/><br />
-            </form>
 
-            
+            <?php if($_SESSION['isAdmin'] == '1') include('employee-signup.php'); ?>
+
             <form action = "" method = "post" class = "form-box">
                 <h3>For Clients</h3>
                 <label>First Name  :</label><input type = "text" name = "first_name" class = "box"/><br /><br />
