7da51e512cf7e955453bc8c4ba07bfcda54d4482
Alessandro <kalessandro14@gmail.com>
2018-11-22 11:06:30 -0500

    bug fix and styling for transfer money

--- client/actions/transfer-money.php
+++ client/actions/transfer-money.php
@@ -20,7 +20,7 @@
             mysqli_query($db, "UPDATE Account SET balance = $from_account_new_balance WHERE account_id = $from_account AND client_id = $client_id");
             
             //add x amount in 'to_account
-            $sql = "SELECT balance FROM Account WHERE account_id = $from_account AND client_id = $client_id";
+            $sql = "SELECT balance FROM Account WHERE account_id = $to_account AND client_id = $client_id";
             $result = mysqli_query($db,$sql);
             $amount_in_to_account = mysqli_fetch_array($result)['balance'];
             $to_account_new_balance = $amount_in_to_account + $transfer_amount;
@@ -30,14 +30,10 @@
             $is_error = true;
         }
     }else{
-        $sql = "SELECT account_id FROM Account WHERE client_id = $client_id";
+        $sql = "SELECT account_id, balance FROM Account WHERE client_id = $client_id";
         $result = mysqli_query($db,$sql);
-        $account_ids = []; 
-
-        #takes the sql querry results and puts it into an arrow
         while(($row = mysqli_fetch_array($result))) {
-            #$account_ids.push($row);
-            array_push($account_ids,$row['account_id']);
+            $account_ids[] = $row;
         }
     }
 
@@ -52,6 +48,23 @@
     .transfer-money-holder{
         display: flex;
     }
+    select{
+        margin: 7px;
+    }
+    form{
+        display: inline-block;
+        text-align: center;
+    }
+    .form-containter{
+        width: 100%;
+        text-align: center;
+    }
+    h3{
+        margin: 0;
+    }
+    #amount{
+        font-size: 15px;
+    }
 </style>
 <body>
 
@@ -62,31 +75,33 @@
 <div class="container" id="main-content">
 	<h2>Transfer Money</h2>
 
-    <form action = "" method = "post">
-        <div>
-            <div>Amount</div>
-            <input type = "number" name = "transfer_amount">
-        </div>
-        <div class = "transfer-money-holder">
+    <div class = "form-containter">
+        <form action = "" method = "post">
             <div>
-                <h3>From</h3>
-                <select name = "from_account">
-                    <?php foreach($account_ids as $account_id): ?>
-                        <option value = "<?php echo $account_id;?>"><?php echo $account_id; ?></option>
-                    <?php endforeach; ?>
-                </select>
+                <div id = "amount">Amount</div>
+                <input type = "number" name = "transfer_amount">
             </div>
-            <div>
-                <h3>To</h3>
-                <select name = "to_account">
-                    <?php foreach($account_ids as $account_id): ?>
-                        <option value = "<?php echo $account_id;?>"><?php echo $account_id; ?></option>
-                    <?php endforeach; ?>
-                </select>
+            <div class = "transfer-money-holder">
+                <div>
+                    <h3>From</h3>
+                    <select name = "from_account">
+                        <?php foreach($account_ids as $account_id): ?>
+                            <option value = "<?php echo $account_id[0];?>"><?php echo $account_id[0]; echo "(".$account_id[1]."$)" ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div>
+                    <h3>To</h3>
+                    <select name = "to_account">
+                        <?php foreach($account_ids as $account_id): ?>
+                            <option value = "<?php echo $account_id[0];?>"><?php echo $account_id[0]; echo "(".$account_id[1]."$)" ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
             </div>
-        </div>
-        <button type = "submit">Transfer</button>
-    </form>
+            <button type = "submit">Transfer</button>
+        </form>
+    </div>
 </div>
 <?php elseif($is_error) : ?>
     <h1>Error Transfering Money</h1>
