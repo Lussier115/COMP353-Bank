4f4d77559d4568417b7f0553f13ff57d5ce5de4c
carlita <labbecarlita@gmail.com>
2018-11-24 17:43:33 -0500

    added line of credit to be counted in bank losses

--- employee/employee-bankBalance.php
+++ employee/employee-bankBalance.php
@@ -18,9 +18,9 @@ $resultProfit = mysqli_query($db, $sqlProfit);
 $sqlPSum = "SELECT SUM( account.balance) as totalProfit FROM interest_rate, account WHERE account.interest_rate_id = interest_rate.interest_rate_id AND (interest_rate.kind_of_service= 'checking' OR interest_rate.kind_of_service= 'saving' )";
 $resultPSum = mysqli_query($db, $sqlPSum);
 
-$sqlLoss = "SELECT DISTINCT account.balance, interest_rate.kind_of_service FROM interest_rate, account WHERE account.interest_rate_id = interest_rate.interest_rate_id AND (interest_rate.kind_of_service= 'credit' OR interest_rate.kind_of_service= 'line_credit' )";
+$sqlLoss = "SELECT DISTINCT account.balance, interest_rate.kind_of_service FROM interest_rate, account WHERE account.interest_rate_id = interest_rate.interest_rate_id AND (interest_rate.kind_of_service= 'credit' OR interest_rate.kind_of_service= 'credit_line' )";
 $resultLoss = mysqli_query($db, $sqlLoss);
-$sqlLSum = "SELECT SUM( account.balance) as totalLoss FROM interest_rate, account WHERE account.interest_rate_id = interest_rate.interest_rate_id AND (interest_rate.kind_of_service= 'credit' OR interest_rate.kind_of_service= 'line_credit  ' )";
+$sqlLSum = "SELECT SUM( account.balance) as totalLoss FROM interest_rate, account WHERE account.interest_rate_id = interest_rate.interest_rate_id AND (interest_rate.kind_of_service= 'credit' OR interest_rate.kind_of_service= 'credit_line' )";
 $resultLSum = mysqli_query($db, $sqlLSum);
 
 
@@ -63,7 +63,7 @@ $resultLSum = mysqli_query($db, $sqlLSum);
             if ($resultPSum->num_rows > 0) {
                 while ($row = $resultPSum->fetch_assoc()) {
                     echo nl2br("\r\n ");
-                    echo "<tr><th>"."total profit  "."</th><th>" . $row['totalProfit'] . "$" . "</th></tr>";
+                    echo "<tr><th>"."total profit  "."</th><th>" ."+". $row['totalProfit'] . "$" . "</th></tr>";
                 }
             }
             echo "</table>";
@@ -79,7 +79,7 @@ $resultLSum = mysqli_query($db, $sqlLSum);
                 while ($row = $resultLoss->fetch_assoc()) {
 //                    echo nl2br("\r\n ");
                     //  echo $row["kind_of_service"] . ": " . $row["balance"] . "  type of account:  " . $row["type_of_account"];
-                    echo "<tr><td>" . $row['balance'] . "$" . "</td><td>" . $row['kind_of_service'] . "</td></tr>";
+                    echo "<tr><td>" .$row['balance'] . "$" . "</td><td>" . $row['kind_of_service'] . "</td></tr>";
 
                 }
             } else {
@@ -92,12 +92,11 @@ $resultLSum = mysqli_query($db, $sqlLSum);
             if ($resultLSum->num_rows > 0) {
                 while ($row = $resultLSum->fetch_assoc()) {
                     echo nl2br("\r\n ");
-                    echo "<tr><th>"."total loss  "."</th><th>" . $row['totalLoss'] . "$" . "</th></tr>";
+                    echo "<tr><th>"."total loss  "."</th><th>" . "-".$row['totalLoss'] . "$" . "</th></tr>";
                 }
             }
             echo "</table>";
             ?>
-
 
 
 
