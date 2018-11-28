906e47fffe3e19c8133aeef8c0579c6a6f385cb9
SAA9 <s.ali3@protonmail.com>
2018-11-28 15:54:33 -0500

    Set the CSS Style for e-transfer

---
+++ client/actions/e-transfer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("../../includes/head-tag-contents.php");?>
+</head>
+<style>
+    .transfer-money-holder{
+        display: flex;
+    }
+    select{
+        margin: 7px;
+    }
+    form{
+        display: inline-block;
+        text-align: center;
+    }
+    .form-containter{
+        width: 100%;
+        text-align: center;
+    }
+    h3{
+        margin: 0;
+    }
+    #amount{
+        font-size: 15px;
+    }
+</style>
